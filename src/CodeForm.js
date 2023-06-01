94e86c146b938c94053594e3151aa2b430f240be
Ching Lee <qaz741qhn@gmail.com>
2023-06-01 15:08:53 +0900

    add typewriter-effect, edit README

--- src/CodeForm.js
+++ src/CodeForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import TypeWriter from 'typewriter-effect';
 import axios from 'axios';
 import styled from 'styled-components';
 
@@ -8,6 +9,8 @@ const Form = styled.form`
   align-items: center;
   gap: 20px;
   padding: 20px;
+  max-width: 800px;
+  margin: 0 auto;
 `;
 
 const TextArea = styled.textarea`
@@ -15,27 +18,33 @@ const TextArea = styled.textarea`
   height: 150px;
   padding: 10px;
   font-size: 16px;
+  border-radius: 5px;
+  border: 1px solid #ddd;
 `;
 
 const Button = styled.button`
-  width: 100px;
-  padding: 10px;
+  padding: 10px 20px;
   font-size: 16px;
-  color: white;
-  background-color: #3b82f6;
+  color: #282c34;
+  background-color: #61dafb;
   border: none;
   border-radius: 5px;
   cursor: pointer;
+  &:hover {
+    background-color: #282c34;
+    color: #61dafb;
+  }
 `;
 
 const CodeBlock = styled.pre`
-  width: 100%;
-  background-color: #1e1e1e;
-  color: #dcdcdc;
+  background-color: #282c34;
+  color: #abb2bf;
+  border-radius: 5px;
   padding: 10px;
-  overflow-x: auto;
-  white-space: pre-wrap;
-  word-wrap: break-word;
+  font-family: 'Courier New', monospace;
+  height: 300px;
+  overflow: auto;
+  white-space: pre;
 `;
 
 function CodeForm() {
@@ -52,7 +61,17 @@ function CodeForm() {
     <Form onSubmit={handleSubmit}>
       <TextArea value={request} onChange={(e) => setRequest(e.target.value)} />
       <Button type="submit">Generate Code</Button>
-      <CodeBlock>{response && response.generated_code}</CodeBlock>
+      <CodeBlock>
+        {response && (
+          <TypeWriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(response.generated_code)
+                .start();
+            }}
+          />
+        )}
+      </CodeBlock>
     </Form>
   );
 }
